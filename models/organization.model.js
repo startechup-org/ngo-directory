5e2e06a0297f0f7904bf9a24ab603d6ce6f59ec9
Cleo Credo <credocleo28@gmail.com>
2020-07-24 23:38:36 +0800

    fields for org and user models

--- models/organization.model.js
+++ models/organization.model.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
 
-
 const OrganizationSchema = new mongoose.Schema({
+	org_name: String,
+	org_description: String,
+	org_country: String,
+	org_city: String,
+	org_picture: String,
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	updatedAt: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
-const Organization = mongoose.model('organization', OrganizationSchema, 'organization');
+OrganizationSchema.pre('findOneAndUpdate', async function () {
+	this.update({}, { $set: { updatedAt: new Date() } });
+});
+
+const Organization = mongoose.model(
+	'organization',
+	OrganizationSchema,
+	'organization'
+);
 
 module.exports = Organization;
 /*
--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,42 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
+const UserSchema = new mongoose.Schema(
+	{
+		username: String,
+		name: String,
+		email: String,
+		password: String,
+		language: String,
+		country: String,
+		userType: {
+			type: String,
+			enum: ['user', 'ngo_admin', 'super_admin'],
+			default: 'user',
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
+		updatedAt: {
+			type: Date,
+			default: Date.now,
+		},
+	},
+	{
+		versionKey: false,
+	}
+);
 
-const UserSchema = new mongoose.Schema({
+UserSchema.pre('save', async function (next) {
+	const user = this;
+	user.password =
+		user.password && (await bcrypt.hash(user.password.trim(), 12));
+	next();
+});
+
+UserSchema.pre('findOneAndUpdate', async function () {
+	this.update({}, { $set: { updatedAt: new Date() } });
 });
 
 const User = mongoose.model('user', UserSchema, 'user');
