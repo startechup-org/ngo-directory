c2344860310b60ce84eea7f5cadc2bd02bf2767e
Cleo Credo <credocleo28@gmail.com>
2020-09-08 07:15:19 +0800

    added facebook passport strategy

--- passport-config.js
+++ passport-config.js
@@ -1,75 +1,146 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 const User = require('./models/user.model');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-passport.serializeUser(function(user, done) {
-    done(null, user);
-  });
-passport.deserializeUser(function(id, done) {
-    done(null, user);
+passport.serializeUser(function (user, done) {
+	done(null, user);
+});
+passport.deserializeUser(function (id, done) {
+	done(null, user);
 });
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_OAUTH_CALLBACK
-}, async (accessToken, refreshToken, profile, done) => {  //replace cb with done
-    try {
-        //check whether this current user exist in the db
-        const existingUser = await User.findOne({ "google.id": profile.id })
-        if (existingUser) {
-            console.log('User already exists in our DB')
-            const access_token = jwt.sign(existingUser.toJSON(), process.env.SECRET_TOKEN, {
-                expiresIn: '24h',
-            });
-            console.log('access token existing user: ', access_token)
-            return done(null, access_token)
-        } else {
-            //if no existing account, create a new account
-            const newUser = new User({
-                method: 'google',
-                email: profile.emails[0].value,
-                google: {
-                    id: profile.id,
-                }
-            })
-            await newUser.save()
-            const access_token = jwt.sign(newUser.toJSON(), process.env.SECRET_TOKEN, {
-                expiresIn: '24h',
-            });
-            console.log('access token new user: ', access_token)
-        }
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+			callbackURL: process.env.GOOGLE_OAUTH_CALLBACK,
+		},
+		async (accessToken, refreshToken, profile, done) => {
+			//replace cb with done
+			try {
+				//check whether this current user exist in the db
+				const existingUser = await User.findOne({ 'google.id': profile.id });
+				if (existingUser) {
+					console.log('User already exists in our DB');
+					const access_token = jwt.sign(
+						existingUser.toJSON(),
+						process.env.SECRET_TOKEN,
+						{
+							expiresIn: '24h',
+						}
+					);
+					console.log('access token existing user: ', access_token);
+					return done(null, access_token);
+				} else {
+					//if no existing account, create a new account
+					const newUser = new User({
+						method: 'google',
+						email: profile.emails[0].value,
+						google: {
+							id: profile.id,
+						},
+					});
+					await newUser.save();
+					const access_token = jwt.sign(
+						newUser.toJSON(),
+						process.env.SECRET_TOKEN,
+						{
+							expiresIn: '24h',
+						}
+					);
+					console.log('access token new user: ', access_token);
+					return done(null, access_token);
+				}
+			} catch (error) {
+				done(error, false, error.message);
+			}
+		}
+	)
+);
 
-        return done(null, access_token);
+passport.use(
+	new FacebookStrategy(
+		{
+			clientID: process.env.FACEBOOK_OAUTH_CLIENT_ID,
+			clientSecret: process.env.FACEBOOK_OAUTH_CLIENT_SECRET,
+			callbackURL: process.env.FACEBOOK_OAUTH_CALLBACK,
+			profileFields: ['id', 'emails', 'name'],
+		},
+		async (accessToken, refreshToken, profile, done) => {
+			//replace cb with done
+			try {
+				//check whether this current user exist in the db
+				console.log('profile: ', profile);
+				const existingUser = await User.findOne({ facebookId: profile.id });
+				console.log('existingUser: ', existingUser);
+				if (existingUser) {
+					console.log('User already exists in our DB');
+					const access_token = jwt.sign(
+						existingUser.toJSON(),
+						process.env.SECRET_TOKEN,
+						{
+							expiresIn: '24h',
+						}
+					);
+					console.log('access token existing user: ', access_token);
+					return done(null, access_token);
+				} else {
+					//if no existing account, create a new account
+					const newUser = new User({
+						method: 'facebook',
+						email: profile.emails[0].value,
+						google: {
+							id: profile.id,
+						},
+					});
+					await newUser.save();
+					const access_token = jwt.sign(
+						newUser.toJSON(),
+						process.env.SECRET_TOKEN,
+						{
+							expiresIn: '24h',
+						}
+					);
+					console.log('access token new user: ', access_token);
+					return done(null, access_token);
+				}
+			} catch (error) {
+				done(error, false, error.message);
+			}
+		}
+	)
+);
 
-    } catch (error) {
-        done(error, false, error.message)
-    }
-  }
-));
-
-passport.use(new LocalStrategy({
-    usernameField: 'email'
-}, async(email, password, done) => {
-    try{
-         //Find the user given the email
-        const user = await User.findOne({email})
-        //If not, handle it
-        if(!user){
-            return done(null, false)
-        }
-        //Check if the password is correct
-        const valid = user.password && (await bcrypt.compare(password, user.password));
-        //If not, handle it
-        if(!valid){
-            return done(null, false)
-        }
-        //Otherwise, return the user
-        done(null, user)
-    } catch (error) {
-        done(error, false)
-    }
-}))
+passport.use(
+	new LocalStrategy(
+		{
+			usernameField: 'email',
+		},
+		async (email, password, done) => {
+			try {
+				//Find the user given the email
+				const user = await User.findOne({ email });
+				//If not, handle it
+				if (!user) {
+					return done(null, false);
+				}
+				//Check if the password is correct
+				const valid =
+					user.password && (await bcrypt.compare(password, user.password));
+				//If not, handle it
+				if (!valid) {
+					return done(null, false);
+				}
+				//Otherwise, return the user
+				done(null, user);
+			} catch (error) {
+				done(error, false);
+			}
+		}
+	)
+);
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/user.controller');
 const { isAuthenticated } = require('../utils/middleware');
-const passport = require('passport')
-require('../passport-config') //not putting in a variable so that we can quicky use it
-const passportLogin = passport.authenticate('local', {session: false})
+const passport = require('passport');
+require('../passport-config'); //not putting in a variable so that we can quicky use it
+const passportLogin = passport.authenticate('local', { session: false });
 
 router.post('/user/login', passportLogin, UserController.Login);
 router.post('/user/logout', UserController.Logout);
@@ -16,13 +16,36 @@ router.get(
 );
 
 //GOOGLE AUTH
-router.get('/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
-router.get('/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/fail' }),
-  function(req, res) {
-    return res.status(200).json({ message: 'Ok', access_token:  req._passport.session.user});
-  });
+router.get(
+	'/google',
+	passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+router.get(
+	'/google/callback',
+	passport.authenticate('google', { failureRedirect: '/fail' }),
+	function (req, res) {
+		return res
+			.status(200)
+			.json({ message: 'Ok', access_token: req._passport.session.user });
+	}
+);
+
+router.get(
+	'/facebook',
+	passport.authenticate('facebook', { scope: ['email'] })
+);
+
+router.get(
+	'/facebook/callback',
+	passport.authenticate('facebook', { failureRedirect: '/fail' }),
+	function (req, res) {
+		// Successful authentication, redirect home.
+		console.log('user: ', req._passport.session);
+		return res
+			.status(200)
+			.json({ message: 'Ok', access_token: req._passport.session.user });
+	}
+);
 
 router.use(isAuthenticated);
 router.get('/users', UserController.GetAllUsersList);
